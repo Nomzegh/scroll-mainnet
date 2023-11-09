3aadad09d9e4ea5ef608b761f4de5f638c660c73
Anatolii <101575015+Nomzegh@users.noreply.github.com>
2023-11-09 14:09:21 +0100

    Create BasicToken.sol

---
+++ contracts/BasicToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract BasicToken {
+    mapping(address => uint256) public balances;
+
+    function transfer(address to, uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+}
